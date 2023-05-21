24100295da1b34ff9d93c3bf45eea80e361591e4
KrishaManandhar <98373496+KrishaManandhar@users.noreply.github.com>
2023-05-21 10:18:10 +0545

    Create 3_for_loop.sh

---
+++ 3_for_loop.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+for i in 1 2 3 4
+do 
+    echo Number is $i
+done
+
+# for range
+for i in {1..10}
+do 
+    echo Number is $i
+done
+
+for task in work code sleep eat
+do 
+    echo My task is $task
+done
